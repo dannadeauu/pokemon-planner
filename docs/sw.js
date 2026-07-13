71de3df8b6917bb13891afc28cfda4e2362062b6
dannadeauu <daniellenadeau42@gmail.com>
2026-07-13 07:19:23 -0400

    move task bar up

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service worker: precache the app shell, runtime-cache sprites so the app
 // works offline after first load. Bump VERSION whenever shell files change.
-const VERSION = "myteam-v9";
+const VERSION = "myteam-v10";
 
 const SHELL = [
   "./",
