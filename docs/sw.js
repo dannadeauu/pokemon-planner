692ecb8350ef434b87ec09476a7612e9f280e322
dannadeauu <daniellenadeau42@gmail.com>
2026-08-30 12:04:09 -0400

    outline task add

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service worker: precache the app shell, runtime-cache sprites so the app
 // works offline after first load. Bump VERSION whenever shell files change.
-const VERSION = "myteam-v141";
+const VERSION = "myteam-v142";
 
 const SHELL = [
   "./",
